73fa1f4cab6e45a2900e0d14a5c80c2b508669b5
Arsonist-Cook <menezescleiton@gmail.com>
2021-02-25 22:31:41 -0300

    Add Quantity base controls with interaction

--- User Input/Just Kotlin/app/src/main/java/com/example/android/justkotlin/MainActivity.kt
+++ User Input/Just Kotlin/app/src/main/java/com/example/android/justkotlin/MainActivity.kt
@@ -4,19 +4,44 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+    var numberOfCoffees: Int = 0
+    var priceOfCoffee: Double = 5.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        display(numberOfCoffees)
+        displayPrice(numberOfCoffees * priceOfCoffee)
     }
 
-    fun submitOrder(view:View){
-        display(1)
+    fun submitOrder(view: View) {
+        display(numberOfCoffees)
+        displayPrice(numberOfCoffees * priceOfCoffee)
     }
 
     private fun display(number: Int) {
         val quantityTextView: TextView = findViewById(R.id.order_text_input)
         quantityTextView.text = "$number"
     }
+
+    private fun displayPrice(number: Double) {
+        val priceTextView: TextView = findViewById(R.id.price_text_input)
+        priceTextView.text = NumberFormat.getCurrencyInstance().format(number)
+    }
+
+    fun incrementCup(view: View) {
+        numberOfCoffees++
+        display(numberOfCoffees)
+    }
+
+    fun decrementCup(view: View) {
+        if (numberOfCoffees > 0) {
+            numberOfCoffees--
+            display(numberOfCoffees)
+        }
+    }
+
 }
