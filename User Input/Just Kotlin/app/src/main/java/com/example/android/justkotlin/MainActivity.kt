0fa477dd47b8b719c7bd86aa80ddcea5040bcb61
Arsonist-Cook <menezescleiton@gmail.com>
2021-02-25 21:54:13 -0300

    Add First Button Interaction

--- User Input/Just Kotlin/app/src/main/java/com/example/android/justkotlin/MainActivity.kt
+++ User Input/Just Kotlin/app/src/main/java/com/example/android/justkotlin/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.android.justkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun submitOrder(view:View){
+        display(1)
+    }
+
+    private fun display(number: Int) {
+        val quantityTextView: TextView = findViewById(R.id.order_text_input)
+        quantityTextView.text = "$number"
     }
 }
